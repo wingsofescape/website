f74f117d729a702356f1baa9c9fd69ffc3006955
Vincent <aavincent@deloitte.com>
2025-08-12 22:02:13 +0530

    Changed the migration generated location to default now the import for prismaClient is as default @prisma/client

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
