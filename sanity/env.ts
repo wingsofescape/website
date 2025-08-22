a31191eac5ca6862a9a52cfbf192178513166950
Aabhas Vincent <aabhasvincent@gmail.com>
2025-08-22 15:42:33 +0530

    Checking sanity on prod for the first time

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,7 @@
 // Load environment variables
 
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-08-19";
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-08-22";
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
--- sanity/schemaTypes/heroBannerType.ts
+++ sanity/schemaTypes/heroBannerType.ts
@@ -17,16 +17,23 @@ export const heroBannerType = defineType({
       name: "heroBannerButtons",
       type: "array",
       of: [
-        defineField({
-          name: "heroBannerButton",
-          type: "object",
-          fields: [
-            defineField({
-              name: "link",
-              type: "url",
-            }),
-          ],
-        }),
+       {
+              type: 'object', // This indicates an object within the array
+              name: 'buttnos', // A unique name for this object type
+              fields: [
+                {
+                  name: 'title',
+                  title: 'Title',
+                  type: 'string',
+                },
+                {
+                  name: 'link',
+                  title: 'Link',
+                  type: 'url',
+                },
+                // Add more fields for your object as needed
+              ],
+            },
       ],
     }),
     defineField({
