b8d248a2fdeec64191fde12c12716651b1fe4575
AabahsVincent <aabhasvincent@gmail.com>
2025-08-28 17:03:20 +0530

    Tour schema temporary

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { heroBannerType } from "./heroBannerType";
 import { destinationType } from "./destinationType";
+import { tourType } from "./tourType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [heroBannerType, destinationType],
+  types: [heroBannerType, destinationType, tourType],
 };
