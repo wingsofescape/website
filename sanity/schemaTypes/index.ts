f41eff91795e641e647c45c2cd212349da40b377
WingsOfEscape <wingsofescape@gmail.com>
2025-09-17 14:30:33 +0530

    Additional schemas for Top 10 and where to go by month (#33)
    Co-authored-by: Aabhas Vincent <aabhasvincent@gmail.com>

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -3,7 +3,9 @@ import { destinationType } from "./destinationType";
 import { tourType } from "./tourType";
 import { HomePageType } from "./homePageType";
 import { blogType } from "./blogType";
+import { top10Types } from "./top10Types";
+import { byMonthTypes } from "./byMonthTypes";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [HomePageType, destinationType, tourType, blogType],
+  types: [HomePageType, destinationType, tourType, blogType, top10Types, byMonthTypes],
 };
