a76eba31a4e01cd5238acb46429fd4d4595612a9
AabahsVincent <aabhasvincent@gmail.com>
2025-09-25 16:36:00 +0530

    Contact us styling

--- app/(root)/contact-us/page.tsx
+++ app/(root)/contact-us/page.tsx
@@ -2,26 +2,27 @@ import React from "react";
 
 const ContactUs = () => {
   return (
-    <div className="flex flex-col justify-between items-center mx-auto w-full flex-wrap p-5 md:p-0">
-      <h1 className="text-3xl text-theme-primary font-bold my-5">Contact US</h1>
+    <div className="flex flex-col justify-between items-center mx-auto w-full flex-wrap p-5 md:pb-10 bg-linear-to-b from-theme-primary-dark to-white text-white">
+      <h1 className="text-3xl text-white font-bold my-5">Contact US</h1>
       <p>
         {" "}
         All those who wander are not lost. In case you are, we are here to help.
       </p>
 
-      <h2 className="text-xl text-theme-primary font-bold mt-15 mb-5">
+      <h2 className="text-xl text-white font-bold mt-15 mb-5">
         {" "}
         Quick Support
       </h2>
-      <div className="card-container flex flex-col md:flex-row gap-2 md:gap-8 w-full md:w-2/3 p-8">
-        <div className="card rounded-2xl w-full shadow p-8 md:h-60 md:w-1/3 flex flex-row-reverse md:flex-col  justify-between">
+      <div className="card-container flex flex-col md:flex-row gap-4 md:gap-8 w-full md:w-2/3 p-8">
+        <div className="card rounded-2xl ring w-full shadow p-6 md:h-50 md:w-1/3 flex flex-row-reverse md:flex-col justify-between relative">
+          {/* <div className="overlay bg-slate-100 h-full w-full absolute opacity-20 top-0 left-0 rounded-2xl"></div> */}
           <div className="iconContainer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-telephone text-theme-primary h-10 w-10"
+              className="bi bi-telephone text-white h-10 w-10"
               viewBox="0 0 16 16"
             >
               <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
@@ -29,17 +30,17 @@ const ContactUs = () => {
           </div>
           <div className="contentBox">
             <h3 className="text-md font-semibold mb-5">Call us</h3>
-            <p>+91 -9988766775</p>
+            <p className="text-xs text-theme-primary-light">+91 -9988766775</p>
           </div>
         </div>
-        <div className="card rounded-2xl w-full shadow p-8 md:h-60 md:w-1/3 flex flex-row-reverse md:flex-col  justify-between">
+        <div className="card rounded-2xl ring w-full shadow p-6 md:h-50 md:w-1/3 flex flex-row-reverse md:flex-col  justify-between">
           <div className="iconContainer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-whatsapp text-theme-primary h-10 w-10"
+              className="bi bi-whatsapp text-white h-10 w-10"
               viewBox="0 0 16 16"
             >
               <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
@@ -47,17 +48,17 @@ const ContactUs = () => {
           </div>
           <div className="contentBox">
             <h3 className="text-md font-semibold mb-5">Whatsapp us</h3>
-            <p>+91 -9988766775</p>
+            <p className="text-xs text-theme-primary-light">+91 -9988766775</p>
           </div>
         </div>
-        <div className="card rounded-2xl w-full shadow p-8 md:h-60 md:w-1/3 flex flex-row-reverse md:flex-col justify-between">
+        <div className="card rounded-2xl ring w-full shadow p-6 md:h-50 md:w-1/3 flex flex-row-reverse md:flex-col justify-between">
           <div className="iconContainer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              className="bi bi-envelope-at text-theme-primary h-10 w-10"
+              className="bi bi-envelope-at text-white h-10 w-10"
               viewBox="0 0 16 16"
             >
               <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2zm3.708 6.208L1 11.105V5.383zM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2z" />
@@ -66,7 +67,9 @@ const ContactUs = () => {
           </div>
           <div className="contentBox">
             <h3 className="text-md font-semibold mb-5">Email us</h3>
-            <p className="break-words ">wingsofescale@gmail.com</p>
+            <p className="break-words text-xs text-theme-primary-light">
+              wingsofescape@gmail.com
+            </p>
           </div>
         </div>
       </div>
