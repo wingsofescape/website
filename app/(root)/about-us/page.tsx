3ce1875ca33fd5927c6f0d31682efabe606cc886
Aabhas Vincent <aabhasvincent@gmail.com>
2025-10-17 00:03:41 +0530

    Build fix

--- app/(root)/about-us/page.tsx
+++ app/(root)/about-us/page.tsx
@@ -2,19 +2,16 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="container flex flex-col justify-between items-center mx-auto w-full flex-wrap p-5 md:pb-10  text-theme-primary-dark">
-      <h1 className="text-3xl  font-bold my-5">About US</h1>
+    <div className="container flex flex-col justify-between items-center mx-auto w-full flex-wrap p-5 md:pb-10 text-theme-primary-dark">
+      <h1 className="text-3xl font-bold my-5">About US</h1>
       <p>
         {" "}
         At Wings of Escape, we believe that travel is more than just a getaway—it's an invitation to rediscover the world, ourselves, and the joy of discovery. Born out of a shared desire to create meaningful, delightful, and well-planned escapes from the monotony of everyday life, Wings of Escape is a travel company built for those who seek more than just a destination.
         With over 100 cities explored across the globe—both solo and with diverse groups of travelers—our founders have witnessed firsthand the gaps between what travelers truly need and the often lackluster services offered by the industry. Whether you're seeking adventure, relaxation, or cultural immersion, we've been there, felt it, and know what it takes to craft the perfect journey.
-        Our mission is simple: to provide you with the wings to soar above the ordinary and experience travel in a way that’s as unique as you are. We combine thoughtful planning, personal experiences, and a deep understanding of your travel desires to create escapes that leave you feeling rejuvenated, inspired, and deeply connected to the world.
-        Join us at Wings of Escape, where every trip is more than just a vacation—it’s a chance to transform, to explore, and to escape to something extraordinary
+        Our mission is simple: to provide you with the wings to soar above the ordinary and experience travel in a way that's as unique as you are. We combine thoughtful planning, personal experiences, and a deep understanding of your travel desires to create escapes that leave you feeling rejuvenated, inspired, and deeply connected to the world.
+        Join us at Wings of Escape, where every trip is more than just a vacation—it's a chance to transform, to explore, and to escape to something extraordinary
         Let us be the wings to your next great escape.
-
       </p>
-
-
     </div>
   );
 };
